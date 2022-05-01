f53c79b60325ad551c873b7620ae1fba5dd2c39b
Jrana1 <jewelbutex12@gmail.com>
2022-05-01 23:06:01 +0200

    first commit

---
+++ ch152.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "Ihre name eingeben:";
+    char name[20];
+    cin >> name;
+    cout << "Welcome to Cpp Herr " << name << endl;
+
+    return 0;
+}
